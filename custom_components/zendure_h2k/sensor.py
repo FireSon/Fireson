c368bb384b27f53bb30ddbb2ea2b2a300fcbfa78
Jan Brandsma <J.Brandsma@bellt.nl>
2025-02-15 09:33:33 +0100

    update

--- custom_components/zendure_h2k/sensor.py
+++ custom_components/zendure_h2k/sensor.py
@@ -29,7 +29,18 @@ async def async_setup_entry(
     """Set up the Sensors."""
     # This gets the data update coordinator from the config entry runtime data as specified in your __init__.py
     coordinator: ZendureCoordinator = config_entry.runtime_data.coordinator
-    coordinator.setSensor = async_add_entities
+
+    # Enumerate all the sensors in your data value from your DataUpdateCoordinator and add an instance of your sensor class
+    # to a list for each one.
+    # This maybe different in your specific case, depending on how your data is structured
+    sensors = [
+        ZendureSensor(coordinator, device)
+        for device in coordinator.data.devices
+        if device.device_type == DeviceType.TEMP_SENSOR
+    ]
+
+    # Create the sensors.
+    async_add_entities(sensors)
 
     # Enumerate all the sensors in your data value from your DataUpdateCoordinator and add an instance of your sensor class
     # to a list for each one.
