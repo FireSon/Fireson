72202dc3aaf83d9ce5733e4528d465d9b0606cb9
Jan Brandsma <J.Brandsma@bellt.nl>
2025-02-15 12:35:39 +0100

    upd

--- custom_components/zendure_h2k/coordinator.py
+++ custom_components/zendure_h2k/coordinator.py
@@ -80,6 +80,7 @@ class ZendureCoordinator(DataUpdateCoordinator):
             if not self.api.connected:
                 await self.hass.async_add_executor_job(self.api.connect)
             devices = await self.hass.async_add_executor_job(self.api.get_devices)
+            self.do_callback()
         except APIAuthError as err:
             _LOGGER.error(err)
             raise UpdateFailed(err) from err
